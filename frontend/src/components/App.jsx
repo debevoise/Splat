c76f621aa3959bee040714b569c4faa5ff4e1459
Simon_DeBevoise <simondebevoise@gmail.com>
2019-12-30 13:38:26 -0500

    add audio element render fn

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,20 +6,35 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.samples = [];
+		this.samples = new Array(8);
 	}
 
-	
+	componentDidMount() {}
+
+	// createAudioElementsList() {
+	// 	const audioElements = this.props.theme.samples.map((sampleId, idx) => {
+	// 		const sample = this.props.samples[sampleId];
+	// 		return (
+	// 			<li key={idx} className="audio-elements-list">
+	// 				<h3>{sample.name}</h3>
+	// 				<audio
+	// 					id="sample1"
+	// 					ref={audio => (this.samples[idx] = audio)}
+	// 					src={sample.url}
+	// 				>
+	// 					Your browser does not support audio playback
+	// 				</audio>
+	// 			</li>
+	// 		);
+	// 	});
+
+	// 	return <ul className='samplesList'>{audioElements}</ul>
+	// }
 
 	render() {
 		return (
       <main>
-				<audio 
-					id="sample1" 
-					ref={audio => (this.samples[0] = audio)}
-				>
-          Your browser does not support audio playback
-        </audio>
+
       </main>
     );
 	}
