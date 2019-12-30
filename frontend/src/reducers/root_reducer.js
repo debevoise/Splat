801f4e066bfbe0cb8a42040efa16003b777f5c61
Simon_DeBevoise <simondebevoise@gmail.com>
2019-12-29 19:04:28 -0500

    create root component with provider

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+// import sessionReducer from "./session_reducer";
+// import entitiesReducer from "./entities_reducer";
+
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
