d293f5d8ac72f63b16381e4a4f64cbf6ecd71e23
Hunter <hsmanel@gmail.com>
2019-12-30 14:05:56 -0500

    entities reducer merge conflict resolved

--- frontend/src/actions/sample_actions.js
+++ frontend/src/actions/sample_actions.js
@@ -4,24 +4,31 @@ export const RECEIVE_SAMPLES = 'RECEIVE_SAMPLES';
 export const RECEIVE_SAMPLE = 'RECEIVE_SAMPLE';
 export const RECEIVE_SAMPLE_ERRORS = 'RECEIVE_SAMPLE';
 
-export const receiveSamples = samples => ({
+const receiveSamples = samples => ({
     type: RECEIVE_SAMPLES,
     samples
 })
 
-export const receiveSample = sample => ({
+const receiveSample = sample => ({
     type: RECEIVE_SAMPLE,
     sample
 })
 
-export const receiveErrors = errors => ({
+const receiveErrors = errors => ({
     type: RECEIVE_SAMPLE_ERRORS,
     errors
 })
 
+export const fetchSample = () => dispatch => {
+  return SamplesApiUtil.fetchSample().then(
+    ({ data }) => dispatch(receiveSample(data)),
+    errors => dispatch(receiveErrors(errors))
+  );
+};
+
 export const fetchSamples = () => dispatch => {
     return SamplesApiUtil.fetchSamples().then(
-        (samples) => dispatch(receiveSamples(samples)),
+        ({ data }) => dispatch(receiveSamples(data)),
         (errors) => dispatch(receiveErrors(errors))
     )
 }
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import samplesReducer from './samples_reducer';
 import sequencesReducer from './sequences_reducer';
+import themesReducer from './themes_reducer';
 
 const entitiesReducer = combineReducers({
   samples: samplesReducer,
+  themes: themesReducer,
   sequences: sequencesReducer
 })
 
