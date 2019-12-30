f33939b78106ba88768441738a3841204d632a1b
Simon_DeBevoise <simondebevoise@gmail.com>
2019-12-30 14:59:58 -0500

    etc...

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,27 +9,30 @@ class App extends React.Component {
 		this.samples = new Array(8);
 	}
 
-	componentDidMount() {}
+	componentDidMount() {
 
-	// createAudioElementsList() {
-	// 	const audioElements = this.props.theme.samples.map((sampleId, idx) => {
-	// 		const sample = this.props.samples[sampleId];
-	// 		return (
-	// 			<li key={idx} className="audio-elements-list">
-	// 				<h3>{sample.name}</h3>
-	// 				<audio
-	// 					id="sample1"
-	// 					ref={audio => (this.samples[idx] = audio)}
-	// 					src={sample.url}
-	// 				>
-	// 					Your browser does not support audio playback
-	// 				</audio>
-	// 			</li>
-	// 		);
-	// 	});
 
-	// 	return <ul className='samplesList'>{audioElements}</ul>
-	// }
+	}
+
+	createAudioElementsList() {
+		const audioElements = this.props.theme.samples.map((sampleId, idx) => {
+			const sample = this.props.samples[sampleId];
+			return (
+				<li key={idx} className="audio-elements-list">
+					<h3>{sample.name}</h3>
+					<audio
+						id="sample1"
+						ref={audio => (this.samples[idx] = audio)}
+						src={sample.url}
+					>
+						Your browser does not support audio playback
+					</audio>
+				</li>
+			);
+		});
+
+		return <ul className='samplesList'>{audioElements}</ul>
+	}
 
 	render() {
 		return (
@@ -43,19 +46,3 @@ class App extends React.Component {
 export default App;
 
 
-//  <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
