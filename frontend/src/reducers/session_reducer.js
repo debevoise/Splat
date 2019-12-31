51c9ec1906948ad241b44f20e459cfa708fae684
Simon_DeBevoise <simondebevoise@gmail.com>
2019-12-31 10:10:02 -0500

    change id to _id, test session reducer

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -4,7 +4,7 @@ import { RECEIVE_THEME } from "../actions/themes_actions";
 const sessionReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_THEME:
-            return {currentTheme: action.theme.id}    
+            return {currentTheme: action.theme._id}    
         default:
             return state;
     }
