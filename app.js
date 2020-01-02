79d8d500cfa5a1e9a6a52a441170be8d8a0a7399
Simon_DeBevoise <simondebevoise@gmail.com>
2020-01-02 14:20:00 -0500

    add build script to package.json

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ Mongoose
 const app = express();
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 app.use(express.static('public'))
 app.get("/", (req, res) => res.send("Appy McAppFace"));
 app.use('/api/samples', samples)
