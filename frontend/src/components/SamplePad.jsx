141c66f99a998ece6d86ed5265475d3ec07585d5
Simon_DeBevoise <simondebevoise@gmail.com>
2020-01-02 13:22:20 -0500

    handle keypress started, display same name info on pads

--- frontend/src/components/SamplePad.jsx
+++ frontend/src/components/SamplePad.jsx
@@ -6,6 +6,7 @@ export default class SamplePad extends React.Component {
     super(props);
 
     this.handlePress = this.handlePress.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handlePress(e) {
@@ -18,9 +19,21 @@ export default class SamplePad extends React.Component {
     }
   }
 
+  handleKeyPress(e) {
+    const { audio, keypress } = this.props;
+    debugger
+    if (audio.paused && e.key === keypress) {
+      audio.play();
+    } else {
+      audio.currentTime = 0
+    }
+  }
+
   render() {
     return (
-      <div className="sample" onMouseDown={this.handlePress}>
+      <div className="sample" onMouseDown={this.handlePress} onKeyDown={this.handleKeyPress}>
+        <div>{this.props.sampleInfo.name}</div>
+        <div></div>
       </div>
     )
   }
